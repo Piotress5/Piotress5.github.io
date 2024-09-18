b479c00de7e8cc251ffc29e4cab7241988abd23a
Piotress5 <piotrciesla222@gmail.com>
2024-09-18 03:53:57 +0200

    Update index.html #2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const menu_hover = document.querySelectorAll("li a");
+const main_start_block = document.querySelector("div.main-header-block");
+const main_start_header = document.querySelector("div.main-header-block h1");
 const news_svg = document.querySelectorAll("svg.news-triangle");
 const news_projects_desc = document.querySelectorAll("div.project-desc");
 const news_projects = document.querySelectorAll("div.project-block");
 const projects_objects = document.querySelectorAll("div.project-object");
 const projects_list = document.querySelector("div.project-list");
+const body = document.querySelector("body");
 let licznik = 0;
 let news_wartosc = 0;
 let projects_delay = 0;
@@ -26,8 +29,37 @@ menu_hover.forEach(function() {
     }
 })
 
+// ----------------- Mainpage -----------------
+//animacja wstepna
+body.onload = function entry_animation() {
+    setTimeout(() => {
+        main_start_block.style.opacity = "1";
+        main_start_block.style.setProperty('--welcome_light1', 'left_light');
+        main_start_block.style.setProperty('--welcome_light2', 'right_light');
+        main_start_block.style.setProperty('--repeating', '1');
+        setTimeout(() => {
+            main_start_block.style.setProperty('--welcome_light1', 'flickering');
+            main_start_block.style.setProperty('--welcome_light2', 'flickering');
+            main_start_block.style.setProperty('--duration1', '2s');
+            main_start_block.style.setProperty('--repeating1', 'infinite');
+            setTimeout(() => {
+                main_start_header.style.opacity = "1";
+                main_start_header.style.setProperty('--repeating2', '1');
+                main_start_header.style.setProperty('--repeating3', '1');
+                setTimeout(() => {
+                    main_start_header.style.setProperty('--backlight', 'backflickering');
+                    main_start_header.style.setProperty('--repeating3', 'infinite');
+                    main_start_header.style.setProperty('--duration2', '1s');
+                    main_start_header.style.setProperty('--header', 'skewing')
+                    main_start_header.style.setProperty('--repeating2', 'infinite');
+                }, 1900);
+            }, 100);
+        }, 3800);
+    }, 100);
+}
+
 // ----------------- News -----------------
-//rozwijanie opisu projektów
+//rozwijanie opisu projektow
 news_projects.forEach(function(opis) {
     opis.addEventListener("click", rozwin);
     function rozwin() {
